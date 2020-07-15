c1d779db6efddc60a9bc0da493a9e937798fd9d2
amirmaavia <Maaviyamansoori222@gmail.com>
2020-07-15 17:14:17 +0500

    name change

--- src/components/About.js
+++ src/components/About.js
@@ -48,7 +48,7 @@ function About() {
             
                 <Avatar alt="DP" src="https://scontent.fkhi10-1.fna.fbcdn.net/v/t1.0-9/p720x720/99295298_2580241465626614_6242239664812457984_o.jpg?_nc_cat=101&_nc_sid=110474&_nc_eui2=AeFp2VbsvJAbrlR0EQScs4ooSfvJQWDtV-JJ-8lBYO1X4hJP28I8AuV5tR2wIBNiKILUHzxtxsmT8_BvHsemYBk5&_nc_ohc=7xZBPk96A4oAX8GqoCF&_nc_ht=scontent.fkhi10-1.fna&_nc_tp=6&oh=8808cc9f1ce6e80d0200f19a4329ac16&oe=5F34CFB1"
                     className={classes.large} />
-                <Typography className={classes.name} variant="h3">Amir Maavia</Typography>
+                <Typography className={classes.name} variant="h3">Amir Maaviya</Typography>
           
             <Button variant="contained" color="primary"><a style={{textDecoration:"none", color:"white"}} href="https://github.com/amirmaavia/Shopping.git" target="_blank">
         Go to Code
